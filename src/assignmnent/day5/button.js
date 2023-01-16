88d702df67ea28ac63ffac437bad2648c847b3fa
ankit bhargav <ankit.bhargav@moonraft.com>
2023-01-16 13:28:14 +0530

    added bindings

--- src/assignmnent/day5/button.js
+++ src/assignmnent/day5/button.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 
-export default function BasicButtons(props) {
+export default function BasicButtons({disabled,title}) {
 
   return (
-      <Button  variant="contained" type='submit' disabled={props.disabled}>{props.title}</Button>
+      <Button  variant="contained" type='submit' disabled={disabled}>{title}</Button>
   );
 }
